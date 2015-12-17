ba33bd85d2de67b433817d163bf917f8da605412
shanekelly-bbc <shane.kelly@bbc.co.uk>
2015-12-17 15:56:00 +0000

    Update poc.js

--- poc.js
+++ poc.js
@@ -1,3 +1,3 @@
 console.log("I'm here");
 window.frames[0].document.documentElement.innerHTML = 'this from github';
-window.history.pushState(“object or string”, “Title”, “/new-url”);
+window.history.pushState("object or string", "Title", "/new-url");
