89c3de9a897106e5414be2441c95e66b2e1034c3
shanekelly-bbc <shane.kelly@bbc.co.uk>
2015-12-17 15:57:59 +0000

    Update poc.js

--- poc.js
+++ poc.js
@@ -1,3 +1,3 @@
 console.log("I'm here");
 window.frames[0].document.documentElement.innerHTML = 'this from github';
-window.history.pushState("object or string", "Title", "/thisIsTheRealBBC_Honest");
+window.history.pushState("object or string", "Title", "/frameworks/barlesque/webservice/body_first.html?blq_gvl=311");
